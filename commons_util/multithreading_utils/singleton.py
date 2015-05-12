596ec3589c53479181a5d4bf08429db76c794445
Daniel D. ZHANG <zhangdanyangg@gmail.com>
2015-05-12 22:47:57 +0800

    singleton

---
+++ commons_util/multithreading_utils/singleton.py
@@ -0,0 +1,18 @@
+import threading
+__author__ = 'Daniel'
+
+
+class SingletonMixin(object):
+    """
+    https://gist.github.com/werediver/4396488
+    """
+    __singleton_lock = threading.Lock()
+    __singleton_instance = None
+
+    @classmethod
+    def instance(cls):
+        if not cls.__singleton_instance:
+            with cls.__singleton_lock:
+                if not cls.__singleton_instance:
+                    cls.__singleton_instance = cls()
+        return cls.__singleton_instance
