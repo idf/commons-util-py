3d6e107bdcce310da8fbb332e2329b06551cef3f
Daniel D. ZHANG <zhangdanyangg@gmail.com>
2015-08-30 00:44:17 +0800

    method_decorator

--- commons_util/decorators/classes.py
+++ commons_util/decorators/classes.py
@@ -42,3 +42,32 @@ def Deprecated(func, msg=None):
 
     return wrapper_func
 
+
+def method_decorator(decorator):
+    """
+    Adapted from Django
+    Converts a function decorator into a method decorator
+    """
+    # 'func' is a function at the time it is passed to _dec, but will eventually
+    # be a method of the class it is defined on.
+    def _dec(func):
+        def _wrapper(self, *args, **kwargs):
+            @decorator
+            def bound_func(*args2, **kwargs2):
+                return func.__get__(self, type(self))(*args2, **kwargs2)
+            # bound_func has the signature that 'decorator' expects i.e.  no
+            # 'self' argument, but it is a closure over self so it can call
+            # 'func' correctly.
+            return bound_func(*args, **kwargs)
+        # In case 'decorator' adds attributes to the function it decorates, we
+        # want to copy those. We don't have access to bound_func in this scope,
+        # but we can cheat by using it on a dummy function.
+
+        @decorator
+        def dummy(*args, **kwargs):
+            pass
+        functools.update_wrapper(_wrapper, dummy)
+        # Need to preserve any existing attributes of 'func', including the name.
+        functools.update_wrapper(_wrapper, func)
+
+        return _wrapper
