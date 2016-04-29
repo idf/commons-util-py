9b52fb982df926b19304eeb202eb97532daf42d5
Daniel D. ZHANG <zhangdanyangg@gmail.com>
2016-04-29 13:51:18 -0700

    yield and send

--- commons_util/fundamentals/generators.py
+++ commons_util/fundamentals/generators.py
@@ -1,5 +1,23 @@
 """
 Prefer `yield` over returning a ret, where ret = []
+
+# Communication
+Think of it like this, with a generator and no send, it's a one way street
+
+==========       yield      ========
+Generator |   ------------> | User |
+==========                  ========
+But with send, it becomes a two way street
+
+==========       yield       ========
+Generator |   ------------>  | User |
+==========    <------------  ========
+                  send
+
+
+# Yield & Send
+bar = yield foo
+  after yield foo, wait for send(val) to assign val to bar
 """
 __author__ = 'Danyang'
 
@@ -13,3 +31,16 @@ def drange(start, stop, step):
 
 def frange(start, stop, step):
     return list(drange(start, stop, step))
+
+
+def first_coroutine():
+    received = yield
+    while True:
+        received = yield "Received: %s" % str(received)
+
+
+def minimize():
+    current = yield
+    while True:
+        value = yield current
+        current = min(value, current)
